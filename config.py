1dd7b9dccf170a218fa1e5c761e4de5fb05d4dd4
archieyang <overflow.yang@gmail.com>
2013-02-26 23:54:05 +0800

    Tutorial finished.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #configuration
-DATABASE = '/tmp/app.db'
+DATABASE = './app.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
-PASSWORD = 'default'
+PASSWORD = 'admin'
--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, g, render_template, request, redirect, url_for, flash, session
 from contextlib import closing
 
 app = Flask(__name__)
@@ -13,9 +13,52 @@ def init_db():
 			db.cursor().executescript(f.read())
 		db.commit()
 
+
 @app.route('/')
-def hello_world():
-	return 'Hello World'
+def show_entries():
+	cur = g.db.execute('select title, text from entries order by id desc')
+	entries = [dict(title = row[0], text = row[1]) for row in cur.fetchall()]
+	return render_template('show_entries.html', entries = entries)
+
+@app.route('/add', methods = ['POST'])
+def add_entry():
+	if not session.get('logged_in'):
+		abort(401)
+	g.db.execute('insert into entries ( title, text) values (?, ?)',[request.form['title'], request.form['text']])
+	g.db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('Logged in !!!')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('Logged out !')
+	return redirect(url_for('show_entries'))	
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	g.db.close()
+
 if __name__ == '__main__':
 	#app.run(host='0.0.0.0', port=80)
 	app.run()
+
+
